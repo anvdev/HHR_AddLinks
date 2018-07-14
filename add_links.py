4e7543d31d397930a7b1ed5b066d6efe56fd515d
Titov Ivan <risenforce@mail.ru>
2018-07-14 10:54:26 +0500

    Первая версия

---
+++ add_links.py
@@ -0,0 +1,24 @@
+import os
+
+
+def add_links(directory):
+    # Todo: сбор дополнительных файлов в архив
+    for root, folders, files in os.walk(directory):
+        for file in files:
+            if file.endswith('txt'):
+                asset_name = file.replace('.txt', '.otl')
+                path = os.path.join(root, asset_name)
+                if os.path.exists(path):
+                    with open(os.path.join(root, file), 'a', encoding='utf8') as f:
+                        f.write(f'\n\n[Скачать пример|{asset_name}]')
+                    continue
+                asset_name = file.replace('.txt', '.hda')
+                path = os.path.join(root, asset_name)
+                if os.path.exists(path):
+                    with open(os.path.join(root, file), 'a', encoding='utf8') as f:
+                        f.write(f'\n\n[Скачать пример|{asset_name}]')
+
+
+if __name__ == '__main__':
+    examples = r'S:\Houdini 16.5.525\houdini\help\examples'
+    add_links(examples)
